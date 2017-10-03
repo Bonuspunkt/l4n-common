57e01ff3a36ef7b15f6cf9e900a59b27d22022ee
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-10-03 21:41:09 +0200

    remove `defaults`, add debug package

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
-const udpBroadcastMessage = new Buffer('¿l4n?');
-
 module.exports = {
     MessageParser: require('./lib/MessageParser'),
     Resolver: require('./lib/Resolver'),
     Spy: require('./lib/Spy'),
-
-    defaults: {
-        // provider
-        udpServer: {
-            port: 19999,
-            message: udpBroadcastMessage,
-        },
-        tlsServer: {
-            // TLS options
-            port: 20000,
-        },
-
-        // server
-        udpScanner: {
-            message: udpBroadcastMessage,
-            port: 19998,
-            interval: 30e3, // 30sec
-        },
-        tlsClient: {
-            // TLS options
-        },
-        db: {
-            filePath: 'core.db',
-        },
-        httpServer: {
-            sessionStore: {
-                db: 'session.db',
-            },
-        },
-    },
 };
